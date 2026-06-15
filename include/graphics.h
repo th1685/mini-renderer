efd7e12dcca05d93a9e4ab07484dd98213d947bb
th1685 <tomtheclimber@gmail.com>
2026-06-15 21:46:06 +0100

    set default terminal colours

--- include/graphics.h
+++ include/graphics.h
@@ -64,14 +64,15 @@ int setup(PANEL* panelptr) {
 	noecho();
 
 	start_color();
-	init_pair(COLOUR_RED, COLOR_RED, COLOR_BLACK);
-	init_pair(COLOUR_GREEN, COLOR_GREEN, COLOR_BLACK);
-	init_pair(COLOUR_BLUE, COLOR_BLUE, COLOR_BLACK);
-	init_pair(COLOUR_CYAN, COLOR_CYAN, COLOR_BLACK);
-	init_pair(COLOUR_MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(COLOUR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(COLOUR_WHITE, COLOR_WHITE, COLOR_BLACK);
-	init_pair(COLOUR_BLACK, COLOR_BLACK, COLOR_BLACK);
+	use_default_colors();
+	init_pair(COLOUR_RED, COLOR_RED, -1);
+	init_pair(COLOUR_GREEN, COLOR_GREEN, -1);
+	init_pair(COLOUR_BLUE, COLOR_BLUE, -1);
+	init_pair(COLOUR_CYAN, COLOR_CYAN, -1);
+	init_pair(COLOUR_MAGENTA, COLOR_MAGENTA, -1);
+	init_pair(COLOUR_YELLOW, COLOR_YELLOW, -1);
+	init_pair(COLOUR_WHITE, COLOR_WHITE, -1);
+	init_pair(COLOUR_BLACK, COLOR_BLACK, -1);
 
 	return 0;
 }
